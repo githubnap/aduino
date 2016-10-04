f1d546579ab12497c995af71c68ca0d369a699cf
sudtay Onnuam <sudtayonnuam@sudtays-MacBook-Pro.local>
2016-10-04 18:47:16 +0700

    Add code Archery timer

---
+++ Buzzer_at_Archery/Buzzer_at_Archery.ino
@@ -0,0 +1,70 @@
+#include "TM1637.h"
+#define CLK 2      
+#define DIO 3
+TM1637 tm1637(CLK,DIO);
+
+#define BT_pin 6
+#define sound_pin 4     //Active Low 
+#define Light_pin 5     //Active Low
+
+void setup() {
+    Serial.begin(9600);
+    tm1637.init();
+    tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+
+    pinMode(BT_pin,INPUT);
+    pinMode(sound_pin,OUTPUT);
+    pinMode(Light_pin,OUTPUT);
+    
+    digitalWrite(BT_pin,LOW);
+    digitalWrite(sound_pin,HIGH);
+    digitalWrite(Light_pin,HIGH);
+}
+
+void loop() {
+
+  while(digitalRead(BT_pin) == !HIGH){
+    delay(10);
+    digitalWrite(Light_pin,LOW);
+  }
+  
+  for(int i=8; i>0; i--){
+      showLED(i);
+      delay(1000);
+      digitalWrite(Light_pin,LOW);
+  }
+
+  
+  sound(100);
+  digitalWrite(Light_pin,HIGH); //ปิดไฟ
+  
+    for(int i=240; i>=0; i--){
+      showLED(i);
+      delay(1000);
+  }
+
+  digitalWrite(Light_pin,LOW); //เปิดไฟ
+  sound(1000);
+  delay(100);
+  software_Reset() ;// Reset
+
+}
+
+
+void showLED(int num){
+    tm1637.display(0,num/1000);
+    tm1637.display(1,num%1000/100); 
+    tm1637.display(2,num%100/10);
+    tm1637.display(3,num%10);
+}
+
+void sound(int T){
+    digitalWrite(sound_pin,LOW);
+    delay(T);
+    digitalWrite(sound_pin,HIGH);
+}
+
+void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
+{
+  asm volatile ("  jmp 0");
+}
