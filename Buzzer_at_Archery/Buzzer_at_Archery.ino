f4f147cfd24d373a7d6c2c7069530205e609032a
SUDTAY ONNUAM <napsudtay@gmail.com>
2017-04-09 16:57:26 +0700

    แก้ code เพื่อลง pcb
    เปลี่ยน Active Low เป็น Active High ทั้งหมด

--- Buzzer_at_Archery/Buzzer_at_Archery.ino
+++ Buzzer_at_Archery/Buzzer_at_Archery.ino
@@ -4,46 +4,47 @@
 TM1637 tm1637(CLK,DIO);
 
 #define BT_pin 6
-#define sound_pin 4     //Active Low 
-#define Light_pin 5     //Active Low
+#define sound_pin 4     //Active HIGH 
+#define Light_pin 5     //Active HIGH
 
 void setup() {
     Serial.begin(9600);
     tm1637.init();
     tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
-
+    showLED(0000);
+    
     pinMode(BT_pin,INPUT);
     pinMode(sound_pin,OUTPUT);
     pinMode(Light_pin,OUTPUT);
     
     digitalWrite(BT_pin,LOW);
-    digitalWrite(sound_pin,HIGH);
-    digitalWrite(Light_pin,HIGH);
+    digitalWrite(sound_pin,LOW);
+    digitalWrite(Light_pin,LOW);
 }
 
 void loop() {
 
   while(digitalRead(BT_pin) != HIGH){
     delay(10);
-    digitalWrite(Light_pin,LOW);
+    digitalWrite(Light_pin,HIGH);
   }
   
   for(int i=8; i>0; i--){
       showLED(i);
       delay(1000);
-      digitalWrite(Light_pin,LOW);
+      //digitalWrite(Light_pin,HIGH);
   }
 
   
   sound(100);
-  digitalWrite(Light_pin,HIGH); //ปิดไฟ
+  digitalWrite(Light_pin,LOW); //ปิดไฟ
   
     for(int i=240; i>=0; i--){
       showLED(i);
       delay(1000);
       if(digitalRead(BT_pin) == HIGH){
         delay(10);
-        digitalWrite(Light_pin,LOW);
+        digitalWrite(Light_pin,HIGH);
         sound(1000);
         delay(100);
         software_Reset() ;// Reset
@@ -51,7 +52,7 @@ void loop() {
       
   }
 
-  digitalWrite(Light_pin,LOW); //เปิดไฟ
+  digitalWrite(Light_pin,HIGH); //เปิดไฟ
   sound(1000);
   delay(100);
   software_Reset() ;// Reset
@@ -67,9 +68,9 @@ void showLED(int num){
 }
 
 void sound(int T){
-    digitalWrite(sound_pin,LOW);
-    delay(T);
     digitalWrite(sound_pin,HIGH);
+    delay(T);
+    digitalWrite(sound_pin,LOW);
 }
 
 void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
